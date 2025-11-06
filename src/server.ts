ac761d889321abac47438e0b445036e3dadab654
Thauan Fernandes <thauan.fernandes@ufpe.br>
2025-11-06 20:58:33 -0300

    hello world api faculdade

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import faculdadeRoutes from "./routes/FaculdadeRoutes";
+import { AppDataSource } from "./database";
+
+const app = express();
+app.use(express.json());
+app.use("/faculdade", faculdadeRoutes);
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Conexão com o banco de dados estabelecida");
+        app.listen(3000, () => {
+            console.log("Servidor rodando com sucesso na porta 3000");
+        });
+    })
+    .catch((error) => {
+        console.error("Erro ao conectar com o banco de dados:", error);
+    });
