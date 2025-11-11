db4bdf09f6e8927144d26a8290e470844caa78b2
Thauan Fernandes <thauan.fernandes@ufpe.br>
2025-11-11 18:17:08 -0300

    feat: add new endpoints

--- src/controller/FaculdadeController.ts
+++ src/controller/FaculdadeController.ts
@@ -4,8 +4,23 @@ import { Turma } from "../entities/Turma";
 const repoTurma = AppDataSource.getRepository(Turma);
 
 export class FaculdadeController {
-    async criarTurma(nome: string, semestre: string, id_disciplina: number) {
+    async criarTurma(nome: string, semestre: string, id_disciplina: number): Promise<Turma> {
         const turma = repoTurma.create({nome, semestre, id_disciplina});
-        return await repoTurma.save(turma);
+        const response = await repoTurma.save(turma);
+        return response;
+    }
+
+
+    async consultarTurmas(): Promise<Turma[]> {
+        const sql = "SELECT * FROM turma";
+        const turmas = await repoTurma.query(sql);
+        return turmas;
+    }
+
+    async updateturma(turma: Turma): Promise<Turma[]> {
+        const { id, nome, semestre, id_disciplina } = turma;
+        const sql = "UPDATE turma SET nome = $1, semestre = $2, id_disciplina = $3 WHERE id = $4 RETURNING *";
+        const turmas = await repoTurma.query(sql, [nome, semestre, id_disciplina, id]);
+        return turmas;
     }
 }
--- src/routes/FaculdadeRoutes.ts
+++ src/routes/FaculdadeRoutes.ts
@@ -4,10 +4,24 @@ import { FaculdadeController } from "../controller/FaculdadeController";
 const router = Router()
 const faculdadeController = new FaculdadeController();
 
-router.post("/turma", (req, res) => {
+router.post("/turma", async (req, res) => {
     const { nome, semestre, id_disciplina } = req.body;
-    const turma = faculdadeController.criarTurma(nome, semestre, id_disciplina);
+    const turma = await faculdadeController.criarTurma(nome, semestre, id_disciplina);
     res.json(turma);
+});
+
+router.get("/turma", async (_req, res) => {
+    const turmas = await faculdadeController.consultarTurmas();
+    res.json(turmas);
+});
+
+router.put("/turma", async (req, res) => {
+    const turma = req.body;
+    const turmaResponse = await faculdadeController.updateturma(turma);
+    res.json({
+        update: true,
+        turmaResponse
+    });
 });
 
 export default router;
