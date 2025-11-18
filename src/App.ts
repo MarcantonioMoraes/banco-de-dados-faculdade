21427c289af11a086b729e7cc8c80f5faaba7522
Thauan Fernandes <thauan.fernandes@ufpe.br>
2025-11-18 20:38:10 -0300

    add new swagger in application

---
+++ src/App.ts
@@ -0,0 +1,25 @@
+import express, { Express } from "express";
+import { Description, ExpressInitializer, SwaggerEndpoint, SwaggerInitializer, Title, Version } from "express-swagger-autoconfigure";
+import { FaculdadeController } from "./controller/FaculdadeController";
+
+@SwaggerInitializer
+@SwaggerEndpoint("/doc")
+@Description("API de Faculdade")
+@Version("1.0.0")
+@Title("API Faculdade")
+export default class App {
+    @ExpressInitializer
+    public app!: Express;
+    
+    constructor() {
+        this.initControllers();
+    }
+
+    private initControllers() {
+        new FaculdadeController();
+    }
+
+    public getApp() {
+        return this.app;
+    }
+}
